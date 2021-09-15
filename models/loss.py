1905013c6202271c9de776e1b75de0864a7c5fbf
pc3090 <ilikeniku@icloud.com>
2021-09-16 00:51:20 +0900

    update

--- models/loss.py
+++ models/loss.py
@@ -6,10 +6,7 @@ constant_term = 0.5 * math.log(2 * math.pi)
 
 def mle_loss(z, m, log_df_dz, length):
     loss = 0.5 * torch.sum((z - m) ** 2)
-    print(loss)
     loss -= torch.sum(log_df_dz)
-    print(loss)
     loss /= torch.sum(length)
-    print(loss)
     loss += constant_term
     return loss
