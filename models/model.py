f3deb8fd9bed5cfb41f263932f8192ab554db64e
pc3090 <ilikeniku@icloud.com>
2021-09-16 03:58:06 +0900

    update

--- models/model.py
+++ models/model.py
@@ -1,10 +1,12 @@
+import math
 import torch
 import torch.nn as nn
 
 from .common import EmbeddingLayer, RelPositionalEncoding, PreNet
 from .transformer import Transformer
-from .predictors import VarianceAdopter
+from .predictors import VariancePredictor
 from .flow import Glow
+from monotonic_align import maximum_path
 from .utils import sequence_mask, generate_path
 
 
@@ -20,7 +22,7 @@ class TTSModel(nn.Module):
         self.pre_net = PreNet(params.encoder.channels)
         self.encoder = Transformer(**params.encoder)
         self.proj_mu = nn.Conv1d(params.encoder.channels, params.n_mel, 1)
-        self.variance_adopter = VarianceAdopter(**params.variance_adopter)
+        self.duration_predictor = VariancePredictor(**params.variance_predictor)
         self.decoder = Glow(in_channels=params.n_mel, **params.decoder)
 
     def forward(
@@ -30,8 +32,7 @@ class TTSModel(nn.Module):
         f2,
         x_length,
         y,
-        y_length,
-        duration
+        y_length
     ):
         x = self.emb(phoneme, a1, f2)
         x, pos_emb = self.relative_pos_emb(x)
@@ -43,14 +44,21 @@ class TTSModel(nn.Module):
         x = self.encoder(x, pos_emb, x_mask)
         x_mu = self.proj_mu(x)
 
-        attn_mask = torch.unsqueeze(x_mask, -1) * torch.unsqueeze(z_mask, 2)
-        path = generate_path(duration.squeeze(1), attn_mask.squeeze(1))
+        dur_pred = self.duration_predictor(x.detach(), x_mask)
 
-        x_mu, dur_pred = self.variance_adopter(x, x_mu, x_mask, path)
         z, log_df_dz, z_mask = self.decoder(y, z_mask)
         z *= z_mask
-
-        x_mu = x_mu[:, :, :z.size(-1)]
+        attn_mask = torch.unsqueeze(x_mask, -1) * torch.unsqueeze(z_mask, 2)
+        with torch.no_grad():
+            x_logs = torch.zeros_like(x_mu)
+            x_s_sq_r = torch.exp(-2 * x_logs)
+            logp1 = torch.sum(-0.5 * math.log(2 * math.pi) - x_logs, [1]).unsqueeze(-1)  # [b, t, 1]
+            logp2 = torch.matmul(x_s_sq_r.transpose(1, 2), -0.5 * (z ** 2))  # [b, t, d] x [b, d, t'] = [b, t, t']
+            logp3 = torch.matmul(x_mu.transpose(1, 2), z)  # [b, t, d] x [b, d, t'] = [b, t, t']
+            logp4 = torch.sum(-0.5 * (x_mu ** 2), [1]).unsqueeze(-1)  # [b, t, 1]
+            logp = logp1 + logp2 + logp3 + logp4  # [b, t, t']
+            path = maximum_path(logp, attn_mask.squeeze(1)).unsqueeze(1).detach()
+        x_mu = torch.matmul(x_mu, path)
         return x_mu, (z, log_df_dz), dur_pred, (x_mask, z_mask)
 
     def infer(self, phoneme, a1, f2, x_length):
