84053280b107fae32d791769af91bab2104723b4
pc3090 <ilikeniku@icloud.com>
2021-09-16 14:52:42 +0900

    update

--- models/trainer.py
+++ models/trainer.py
@@ -124,7 +124,7 @@ class Trainer:
         x, (z, log_df_dz), dur_pred, (x_mask, y_mask) = model(
             phoneme, a1, f2, x_length, mel, y_length, duration
         )
-        loss_mle = F.mse_loss(x, z) - (torch.sum(log_df_dz) / torch.sum(y_length))
+        loss_mle = mle_loss(z, x, log_df_dz, y_length)
         tgt_dur = torch.log(duration + 1e-4) * x_mask
         loss_duration = F.mse_loss(dur_pred, tgt_dur.to(x.dtype))
         print(loss_mle.dtype, loss_duration.dtype)
