e295f18043d7d97e6a0299021acec90e31b1bcce
pc3090 <ilikeniku@icloud.com>
2021-09-16 12:53:47 +0900

    update

--- models/loss.py
+++ models/loss.py
@@ -1,12 +1,10 @@
 import math
 import torch
 
-constant_term = 0.5 * math.log(2 * math.pi)
 
-
-def mle_loss(z, m, log_df_dz, length):
-    loss = 0.5 * torch.sum((z - m) ** 2)
-    loss -= torch.sum(log_df_dz)
-    loss /= torch.sum(length)
-    loss += constant_term
-    return loss
+def mle_loss(z, m, logdet, length):
+    l = 0.5 * torch.sum((z - m) ** 2)  # neg normal likelihood w/o the constant term
+    l = l - torch.sum(logdet)  # log jacobian determinant
+    l = l / torch.sum(length)  # averaging across batch, channel and time axes
+    l = l + 0.5 * math.log(2 * math.pi)  # add the remaining constant term
+    return l
